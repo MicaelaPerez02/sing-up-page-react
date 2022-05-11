80a2fae4c875e1fa5ff5b7a6cf3de09ddf115378
Micaela Perez <micaelaperez663@gmail.com>
2022-05-10 23:04:26 -0300

    finished and working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import './App.css';
+import { useReducer } from 'react';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  passwordRepeat: '',
+  checkbox: false,
+};
+
+function reducer(state, action){ 
+    return {...state, [action.input]: action.value};
+}
+
+function validateState(state){
+  return state.password === state.passwordRepeat 
+  && state.checkbox
+  && state.password.length > 3;
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function handleClick(event){
+    event.preventDefault();
+    alert(`Hey ${state.name}! you are now registered`);
+  }
+
+  function onChance(event){ 
+    //recive the action and use to update the state
+    const {name, value, checked} = event.target;
+
+    const action = {
+      input : event.target.name,
+      value : name === "checkbox" ? checked : value
+    }
+    dispatch(action);
+  }
+
+
+
   return (
     <div className="App">
       <div className="RegisterFormCont">
         <h2 className="RegisterHeadlineCont"> Register </h2>
           <form className="RegisterForm">
             <input
-              className="TextInput" type="text" name="name" placeholder="Name"
+              className="TextInput" type="text" name="name" placeholder="Name" onChange={onChance}
             />
             <input
-              className="TextInput" type="text" name="email" placeholder="Email"
+              className="TextInput" type="text" name="email" placeholder="Email" onChange={onChance}
             />
             <input
-              className="TextInput" type="password" name="password" placeholder="Password"
+              className="TextInput" type="password" name="password" placeholder="Password" onChange={onChance}
             />
             <input
-              className="TextInput" type="password" name="passwordRepeat" placeholder="Password Repeat"
+              className="TextInput" type="password" name="passwordRepeat" placeholder="Password Repeat" onChange={onChance}
             />
 
             <label className="CheckboxLabel">
               <input 
-                className="Checkbox" type="checkbox" name="checkbox"
+                className="Checkbox" type="checkbox" name="checkbox" onChange={onChance}
               />
               Accept terms of Use:
             </label>
+            <button 
+            disabled={!validateState(state)}
+            onClick={handleClick}
+            className={!validateState (state) ? "Disabled" : null}> 
+            Register 
+            </button>
           </form>
 
 
